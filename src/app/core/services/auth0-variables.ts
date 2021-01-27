6016f7163267ac59c056050bb1ef502f7868b8a7
vaibhav pandey <ex-vaibhav.pandey@optimusinfo.com>
2021-01-27 13:11:08 +0530

    login Auth0 integration

--- src/app/core/services/auth0-variables.ts
+++ src/app/core/services/auth0-variables.ts
@@ -8,8 +8,8 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'OtzXaYOxRZAYv2meHg9N8wl3HWVDts75',
-  domain: 'vaibhav-optimus.us.auth0.com',
+  clientID: '',
+  domain: '',
   callbackURL: 'http://localhost:4200',
   silentCallbackURL: 'http://localhost:3001/silent',
   audience: '{API_IDENTIFIER}',
